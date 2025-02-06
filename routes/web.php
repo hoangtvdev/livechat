5fa24394dcdb4ab528634444a55b75aee2c23573
hoang <hoangtv.dev@gmail.com>
2025-02-06 17:16:50 +0700

    login user and login admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('login', [UserController::class, 'showLoginForm'])->name('login');
+Route::post('login', [UserController::class, 'login']);
+
+Route::middleware(['auth'] /*laravel tự động check theo cấu hình config/auth.php*/)->group(function () {
+    Route::get('/', [UserController::class, 'dashboard'])->name('dashboard');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('login', [AdminController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('login', [AdminController::class, 'login']);
+
+    Route::middleware(['auth:admin'] /*laravel tự động check theo cấu hình config/auth.php*/)->group(function () {
+        Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    });
 });

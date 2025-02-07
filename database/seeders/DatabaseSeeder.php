f3c43b88f4a12849df9a80aca681e2181d2c78ff
hoang <hoangtv.dev@gmail.com>
2025-02-07 10:05:21 +0700

    seeders: admin, user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,44 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $defaultValues = [
+            'password' => Hash::make('123'),
+            'is_active' => true,
+        ];
+
+         \App\Models\User::factory()->create([
+             'name' => 'User',
+             'email' => 'user@gmail.com',
+             'email_verified_at' => now(),
+             ...$defaultValues
+         ]);
+
+        \App\Models\Admin::factory()->createMany(
+            [
+                [
+                    'name' => 'Admin',
+                    'email' => 'admin@gmail.com',
+                    ...$defaultValues
+                ],
+                [
+                    'name' => 'Sale 1',
+                    'email' => 'sale1@gmail.com',
+                    'is_sale' => true,
+                    ...$defaultValues
+                ],
+                [
+                    'name' => 'Sale 2',
+                    'email' => 'sale2@gmail.com',
+                    'is_sale' => true,
+                    ...$defaultValues
+                ],
+                [
+                    'name' => 'Sale 3',
+                    'email' => 'sale3@gmail.com',
+                    'is_sale' => true,
+                    ...$defaultValues
+                ]
+            ]
+        );
     }
 }
